fb90d31a3ea13133688f7a41a5556488a6fd351c
leonardoCaceres <leolima212@hotmail.com>
2022-08-04 19:28:26 -0300

    novos graficos

--- graphs/desvioPadrao.py
+++ graphs/desvioPadrao.py
@@ -18,13 +18,13 @@ newF = pd.DataFrame()
 for file in csv_files:
 	f = pd.read_csv(file)
 	df2 = pd.DataFrame({
-	"Tamanho de pacote" : [int(f["Tamanho de pacote"].std())],
-	"Média de Packet Loss" : [int(f["Packet Loss"].std())],
-	"Média de Loss Percentage(%)" : "0%",		#[f["Packet Loss"].mean()],
-	"Média do Time Delta Mean(s)" : [f["Time Delta Mean(s)"].std()],
-	"Média do Total Time(s)" : [f["Total Time(s)"].std()],
-	"Média de Total Bytes(B)" : [int(f["Total Bytes(B)"].std())],
-	"Média do Throughput(Kbps)" : [f["Throughput(Kbps)"].std()],
+	"Tamanho de pacote" : [int(f["Tamanho de pacote"].mean())],
+	"Desvio Padrão de Packet Loss" : [int(f["Packet Loss"].std())],
+	"Desvio Padrão de Loss Percentage(%)" : "0%",		#[f["Packet Loss"].mean()],
+	"Desvio Padrão do Time Delta Mean(s)" : [f["Time Delta Mean(s)"].std()],
+	"Desvio Padrão do Total Time(s)" : [f["Total Time(s)"].std()],
+	"Desvio Padrão de Total Bytes(B)" : [int(f["Total Bytes(B)"].std())],
+	"Desvio Padrão do Throughput(Kbps)" : [f["Throughput(Kbps)"].std()],
 	})
 	newF = newF.append(df2)
 
--- graphs/plotDesvioPadrao.py
+++ graphs/plotDesvioPadrao.py
@@ -2,37 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as mp
 import glob
 
-for file in csv_files:
-	#print(file)
-	newF = pd.read_csv(file)
-	newF["Tamanho de pacote"] = newF['Total Bytes(B)']/100
-	#print(newF)
-	newF.to_csv(file, index=False)
+dataFile = "desvioPadrao.csv"
+newDF = pd.read_csv(dataFile)
+newDF = newDF.sort_values(by = ['Tamanho de pacote'])
 
-df_list = (pd.read_csv(file) for file in csv_files)
+newDF.plot(x="Tamanho de pacote", y="Desvio Padrão do Throughput(Kbps)", kind="line")
+newDF.plot(x="Tamanho de pacote", y="Desvio Padrão do Time Delta Mean(s)", kind="line")
 
-big_df = pd.concat(df_list)
-big_df = big_df.sort_values(by = ['Total Bytes(B)'])
-
-#print(big_df.to_numpy())
-#print(big_df.columns)
-
-#['Packet Loss', 'Loss Percentage(%)', 'Time Delta Mean(s)',
-#'Total Time(s)', 'Total Bytes(B)', 'Throughput(Kbps)']
-
-big_df.plot(x="Total Bytes(B)", y="Throughput(Kbps)", kind="line")
-big_df.plot(x="Total Bytes(B)", y="Total Time(s)", kind="line")
-
-
-df_list = []
-
-for file in csv_files:
-	df = pd.read_csv(file)
-	df_list.append(df.mean(axis = 0))
-
-newDf = pd.DataFrame(df_list)
-newDf = newDf.sort_values(by = ['Total Bytes(B)'])
-
-newDf.plot(x="Tamanho de pacote", y="Throughput(Kbps)", kind="line")
+#newDF.plot(x="Tamanho de pacote", y="Throughput(Kbps)", kind="line")
 
 mp.show()
